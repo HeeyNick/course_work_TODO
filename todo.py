1a2e6055247fd9ec03d974e21a421009bb5e4113
maksim <jeffrimorgan@yandex.ru>
2020-04-09 15:21:31 +0700

    Fix 'bag' which spinbox dosn't change value

--- todo.py
+++ todo.py
@@ -1,7 +1,5 @@
 import tkinter as tk
-from tkinter import Entry
-from tkinter import Label
-from tkinter import Spinbox
+from tkinter import *
 import sqlite3 as lite
 import datetime
 
@@ -13,26 +11,36 @@ class DB:
 			CREATE TABLE IF NOT EXISTS TODO (
   			№ INTEGER PRIMARY KEY AUTOINCREMENT,
   			Задача TEXT NOT NULL,
- 			Статус TEXT CONST 'Не выполнено',
+ 			Статус TEXT ,
  			Дата_добавления DATE,
  			Дата_окончания DATE
 			)
 			""")
+
 		self.connection.commit()
 
 	def execute_query(self, problem, date_today, Day, Month, Year, problem_str):
-		Year = int(Year)
-		Month = int(Month)
-		Day = int(Day)
+	
 		Date_end = datetime.date(Year, Month, Day)
-		self.cur.execute('''INSERT INTO TODO(Задача, Статус, Дата_добавления, Дата_окончания) VALUES (?,"Не выполнено",?, ?)''',
-                       (problem, date_today, Date_end))
+		
+		status = 'Not_perfomed'
+		self.cur.execute('''INSERT INTO TODO(Задача, Статус, Дата_добавления, Дата_окончания) VALUES (?,?,?, ?)''',
+                       (problem, status, date_today, Date_end))
 		self.connection.commit()
 
-		self.cur.execute("SELECT * FROM TODO ORDER BY № DESC LIMIT 1;")
-		self.result = self.cur.fetchall()
-		print(self.result)
+		self.cur.execute("SELECT № FROM TODO ORDER BY № DESC LIMIT 1;")
+		self.result_number = self.cur.fetchall()
+		self.cur.execute("SELECT Задача FROM TODO ORDER BY № DESC LIMIT 1;")
+		self.result_problem = self.cur.fetchall()
+		self.cur.execute("SELECT Статус FROM TODO ORDER BY № DESC LIMIT 1;")
+		self.result_status = self.cur.fetchall()
+		self.cur.execute("SELECT Дата_окончания FROM TODO ORDER BY № DESC LIMIT 1;")
+		self.result_date_end = self.cur.fetchall()
+		self.cur.execute("SELECT Дата_добавления FROM TODO ORDER BY № DESC LIMIT 1;")
+		self.result_date_now = self.cur.fetchall()
 		problem_str.delete(0,'end')
+		main_win.print_problem(self.result_number, self.result_status, self.result_problem, self.result_date_now, self.result_date_end)
+
 ####################Графическая оболочка###########################
 
 
@@ -46,8 +54,22 @@ class Main_win: #основное окно
 		toolbar = tk.Frame(bg = 'Grey', width = 900, height = 100)
 		toolbar.place ( x =0, y = 0)
 
-
 		self.make_button(toolbar)
+
+	def print_problem(self, result_number, result_status, result_problem, result_date_now, result_date_end):
+		Label(self.root, text=result_number, font = "Times 16").place(x = 50 , y = 150  )
+		Label(self.root, text=result_problem, font = "Times 16").place(x = 90 , y = 150 )
+
+		print(result_status)
+		print(result_date_now)
+		print(type(result_number))
+		print(result_number)
+		number = int(result_number[0][0])
+		print(number)
+		print(type(number))
+		Label(self.root, text=result_status, font = "Times 16").place(x = 150 , y = 150 )
+		Label(self.root, text=result_date_now, font = "Times 16").place(x = 340 , y = 150 )
+		Label(self.root, text=result_date_end, font = "Times 16").place(x = 450 , y = 150 )
 
 	def make_button(self, perent):
 		btn_open_add = tk.Button(perent,
@@ -124,39 +146,41 @@ class Add: #дочернее окно
 		self.root2.geometry("350x130")
 		self.root2.resizable(False, False)
 
+		btn_add_problem = tk.Button(self.root2,
+								text = "Add",
+								width = 5,
+								height = 1,
+								command = lambda:db.execute_query(self.problem.get(),self.date_today,self.Day.get(),self.Month.get(),self.Year.get(),self.problem),
+								bg = "Green",
+								bd=3)
+
+		btn_add_problem.place (x = 100, y = 100)
+
+
 		self.problem = Entry(self.root2, width = 40)
 		self.problem.place(x = 75, y = 5)
+		
 		Label(self.root2, text = 'Задача').place(x = 10, y = 5)
 
 		Label(self.root2, text = 'Дата окончания').place(x = 10, y = 35)
 
 		self.date_today = datetime.date.today()
 
-		self.Day_spin = Spinbox(self.root2, width=3, from_ = 1, to = 31)
+		self.Day = tk.IntVar()
+		self.Month = tk.IntVar()
+		self.Year = tk.IntVar()
+
+		self.Day_spin = Spinbox(self.root2, width=3, from_ = 1, to = 31, textvariable=self.Day)
 		self.Day_spin.place(x = 45, y = 65)
 		Label(self.root2, text = 'День').place(x = 10, y = 65)
 
-		self.Month_spin = Spinbox(self.root2, width = 3, from_=1, to=12)
+		self.Month_spin = Spinbox(self.root2, width = 3, from_=1, to=12, textvariable=self.Month)
 		self.Month_spin.place(x = 135, y = 65)
 		Label(self.root2, text = 'Месяц').place(x = 85, y = 65)
 
-		self.Year_spin = Spinbox(self.root2, width = 5,from_= 2020, to=9999)
+		self.Year_spin = Spinbox(self.root2, width = 5,from_= 2020, to=9999, textvariable=self.Year)
 		self.Year_spin.place(x = 195, y = 65)
 		Label(self.root2, text = 'Год').place(x = 170, y = 65)
-
-		self.Day = self.Day_spin.get()
-		self.Month = self.Month_spin.get()
-		self.Year = self.Year_spin.get()
-
-		btn_add_problem = tk.Button(self.root2,
-								text = "Add",
-								width = 5,
-								height = 1,
-								command = lambda:db.execute_query(self.problem.get(),self.date_today,self.Day,self.Month,self.Year,self.problem),
-								bg = "Green",
-								bd=3)
-
-		btn_add_problem.place (x = 100, y = 100)
 
 		btn_add_problem = tk.Button(self.root2,
 								text = "Close",
@@ -168,12 +192,14 @@ class Add: #дочернее окно
 
 		btn_add_problem.place (x = 190, y = 100)
 
+
 		self.focuse()
 
 	def focuse(self):
 		self.root2.grab_set()
 		self.root2.focus_set()
 		self.root2.wait_window()
+
 
 if __name__ == "__main__":
 	db = DB()
