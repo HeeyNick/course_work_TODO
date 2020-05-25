97bb6f49e5c37bd2a61ac4e703954f422b4c6212
maksim <jeffrimorgan@yandex.ru>
2020-05-26 00:10:18 +0700

    Func expired tasks added

--- db_todo.py
+++ db_todo.py
@@ -1,5 +1,5 @@
 import sqlite3 as lite
-
+import datetime
 
 class DB:
 	def __init__(self, name_db):
@@ -106,3 +106,18 @@ class DB:
 		count_all = len(rows)
 		self.connection.commit()
 		return count_all
+
+	def expired_tasks_bd(self):
+		str_exp_tasks = 'Просрочено на'
+		datetime_today = datetime.date.today()
+		self.cur.execute('''SELECT Дата_окончания, № FROM TODO''')
+		rows_id = self.cur.fetchall()
+		for rows in rows_id:
+			date_end_3 = rows[0]
+			date_end_3 = date_end_3.split('-')
+			date_end_4 = datetime.date(int(date_end_3[0]), int(date_end_3[1]), int(date_end_3[2]))
+			if date_end_4 < datetime_today:
+				date_exp = datetime_today - date_end_4
+				str_exp_tasks = 'Просрочено на ' + str(date_exp.days) + ' д'
+				self.cur.execute('''UPDATE TODO SET Статус = ? WHERE № = ?''', (str_exp_tasks, rows[1]))
+				self.connection.commit()
