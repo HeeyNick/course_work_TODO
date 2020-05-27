f037deeedab61ce7532db5d6b8b613e951536fa4
HeeyNick <nikitos15032002@gmail.com>
2020-05-28 03:23:06 +0700

    made codestile && change travis

--- test_todo.py
+++ test_todo.py
@@ -25,36 +25,36 @@ class test_db(unittest.TestCase):
 
 	def test_aadd_task(self):
 		self.create_db()
-		date_end_test = datetime.date(2020,12,4)
+		date_end_test = datetime.date(2020, 12, 4)
 		date_today_test = datetime.date.today()
 		today_test_str = str(date_today_test)
-		problem ='Трои'
-		self.db.execute_query(problem,date_today_test,date_end_test)
+		problem = 'Трои'
+		self.db.execute_query(problem, date_today_test, date_end_test)
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 1 ''')
 		rows = self.cur.fetchall()
-		self.assertEqual(rows, [('Нет','Трои','Не выполнено',today_test_str,'2020-12-04',1)])
+		self.assertEqual(rows, [('Нет', 'Трои', 'Не выполнено', today_test_str, '2020-12-04', 1)])
 
 	def test_aadd_task_1(self):
 		self.create_db()
-		date_end_test = datetime.date(2020,12,4)
+		date_end_test = datetime.date(2020, 12, 4)
 		date_today_test = datetime.date.today()
 		today_test_str = str(date_today_test)
-		problem ='Семь'
-		self.db.execute_query(problem,date_today_test,date_end_test)
+		problem = 'Семь'
+		self.db.execute_query(problem, date_today_test, date_end_test)
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 2 ''')
 		rows = self.cur.fetchall()
-		self.assertEqual(rows, [('Нет','Семь','Не выполнено',today_test_str,'2020-12-04',2)])
+		self.assertEqual(rows, [('Нет', 'Семь', 'Не выполнено', today_test_str, '2020-12-04', 2)])
 
 	def test_aadd_task_2(self):
 		self.create_db()
-		date_end_test = datetime.date(2020,12,4)
+		date_end_test = datetime.date(2020, 12, 4)
 		date_today_test = datetime.date.today()
 		today_test_str = str(date_today_test)
-		problem ='Восемь'
-		self.db.execute_query(problem,date_today_test,date_end_test)
+		problem = 'Восемь'
+		self.db.execute_query(problem, date_today_test, date_end_test)
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 3 ''')
 		rows = self.cur.fetchall()
-		self.assertEqual(rows, [('Нет','Восемь','Не выполнено',today_test_str,'2020-12-04',3)])
+		self.assertEqual(rows, [('Нет', 'Восемь', 'Не выполнено', today_test_str, '2020-12-04', 3)])
 
 	def test_bcount_fail(self):
 		self.create_db()
@@ -75,7 +75,7 @@ class test_db(unittest.TestCase):
 		self.db.update_performed(id_, status_)
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 2 ''')
 		rows = self.cur.fetchall()
-		self.assertEqual(rows, [('Нет','Семь','Выполнено',today_test_str,'2020-12-04',2)])
+		self.assertEqual(rows, [('Нет', 'Семь', 'Выполнено', today_test_str, '2020-12-04', 2)])
 
 	def test_dcount_pass(self):
 		self.create_db()
@@ -91,7 +91,7 @@ class test_db(unittest.TestCase):
 		self.db.update_priority(id_, prior_)
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 1 ''')
 		rows = self.cur.fetchall()
-		self.assertEqual(rows, [('Важное','Трои','Не выполнено',today_test_str,'2020-12-04',1)])
+		self.assertEqual(rows, [('Важное', 'Трои', 'Не выполнено', today_test_str, '2020-12-04', 1)])
 
 	def test_fcount_important(self):
 		self.create_db()
@@ -103,12 +103,11 @@ class test_db(unittest.TestCase):
 		prior_ = 'Важное'
 		id_ = 1
 		date_today_test = datetime.date.today()
-		today_test_str = str(date_today_test)		
+		today_test_str = str(date_today_test)	
 		self.db.update_priority(id_, prior_)
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 1 ''')
 		rows = self.cur.fetchall()
-		
-		self.assertEqual(rows, [('Нет','Трои','Не выполнено',today_test_str,'2020-12-04',1)])
+		self.assertEqual(rows, [('Нет', 'Трои', 'Не выполнено', today_test_str, '2020-12-04', 1)])
 
 	def test_hupdate_record(self):
 		self.create_db()
@@ -119,8 +118,7 @@ class test_db(unittest.TestCase):
 		self.db.update_record(new_problem, id_)
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 1 ''')
 		rows = self.cur.fetchall()
-		
-		self.assertEqual(rows, [('Нет','Четыре','Не выполнено',today_test_str,'2020-12-04',1)])
+		self.assertEqual(rows, [('Нет', 'Четыре', 'Не выполнено', today_test_str, '2020-12-04', 1)])
 
 	def test_iupdate_record_2(self):
 		self.create_db()
@@ -131,7 +129,7 @@ class test_db(unittest.TestCase):
 		self.db.update_record(new_problem, id_)
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 1 ''')
 		rows = self.cur.fetchall()
-		self.assertEqual(rows, [('Нет','Трои','Не выполнено',today_test_str,'2020-12-04',1)])
+		self.assertEqual(rows, [('Нет', 'Трои', 'Не выполнено', today_test_str, '2020-12-04', 1)])
 
 	def test_jdelete_perf(self):
 		self.create_db()
@@ -163,7 +161,7 @@ class test_db(unittest.TestCase):
 		for id_ in rows_id:
 			if id_[0] != 'Выполнено':
 				datetime_today = datetime.date.today()
-				date_end_test = datetime.date(2020,3,4)
+				date_end_test = datetime.date(2020, 3, 4)
 				exp_date = datetime_today - date_end_test
 				str_exp_tasks = 'Просрочено на ' + str(exp_date.days) + ' д'
 				self.assertEqual(id_[0], (str_exp_tasks))
