744a764cb777287692cf52663d6db5c8f91c36fd
HeeyNick <nikitos15032002@gmail.com>
2020-06-02 13:34:02 +0700

    Make codestyle test_todo.py

--- test_todo.py
+++ test_todo.py
@@ -1,8 +1,7 @@
 import unittest
-import db_todo
 import datetime
 import sqlite3 as lite
-import os
+import db_todo
 
 class test_db_add_delete(unittest.TestCase):
 	def setUp(self):
@@ -23,24 +22,24 @@ class test_db_add_delete(unittest.TestCase):
 		self.connection.commit()
 
 	def test_add_task_1(self):
-		date_end_test = datetime.date(2020,3,4)
+		date_end_test = datetime.date(2020, 3, 4)
 		date_today_test = datetime.date.today()
 		today_test_str = str(date_today_test)
 		problem = 'Трои'
-		self.db.execute_query(problem,date_today_test,date_end_test)
+		self.db.execute_query(problem, date_today_test, date_end_test)
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 1''')
 		rows = self.cur.fetchall()
-		self.assertEqual(rows, [('Нет','Трои','Не выполнено',today_test_str,'2020-03-04',1)])
+		self.assertEqual(rows, [('Нет', 'Трои', 'Не выполнено', today_test_str, '2020-03-04', 1)])
 
 	def test_add_task_2(self):
-		date_end_test = datetime.date(2020,3,4)
+		date_end_test = datetime.date(2020, 3, 4)
 		date_today_test = datetime.date.today()
 		today_test_str = str(date_today_test)
 		problem = 'Ноль'
-		self.db.execute_query(problem,date_today_test,date_end_test)
+		self.db.execute_query(problem, date_today_test, date_end_test)
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 2''')
 		rows = self.cur.fetchall()
-		self.assertEqual(rows, [('Нет','Ноль','Не выполнено',today_test_str,'2020-03-04',2)])
+		self.assertEqual(rows, [('Нет', 'Ноль', 'Не выполнено', today_test_str, '2020-03-04', 2)])
 
 	def test_delete_task_1(self):
 		id_ = 1
@@ -76,7 +75,7 @@ class test_db_update_perf(unittest.TestCase):
 		problem = 'Один'
 		status = 'Не выполнено'
 		date_today = datetime.date.today()
-		date_end = datetime.date(2020,12,4)
+		date_end = datetime.date(2020, 12, 4)
 		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
 		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
 
@@ -86,7 +85,7 @@ class test_db_update_perf(unittest.TestCase):
 		problem = 'Два'
 		status = 'Выполнено'
 		date_today = datetime.date.today()
-		date_end = datetime.date(2020,12,4)
+		date_end = datetime.date(2020, 12, 4)
 		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
 		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
 
@@ -101,10 +100,10 @@ class test_db_update_perf(unittest.TestCase):
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 1 ''')
 		rows = self.cur.fetchall()
 		status_test = 'Выполнено'
-		test_date_end = str(datetime.date(2020,12,4))
+		test_date_end = str(datetime.date(2020, 12, 4))
 		problem = 'Один'
 		priority = 'Нет'
-		self.assertEqual(rows, [(priority,problem,status_test,today_test_str,test_date_end,1)])
+		self.assertEqual(rows, [(priority, problem, status_test, today_test_str, test_date_end, 1)])
 
 	def test_update_perf_task_2(self):
 		status_ = 'Выполнено'
@@ -115,10 +114,10 @@ class test_db_update_perf(unittest.TestCase):
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 2 ''')
 		rows = self.cur.fetchall()
 		status_test = 'Не выполнено'
-		test_date_end = str(datetime.date(2020,12,4))
+		test_date_end = str(datetime.date(2020, 12, 4))
 		problem = 'Два'
 		priority = 'Нет'
-		self.assertEqual(rows, [(priority,problem,status_test,today_test_str,test_date_end,2)])
+		self.assertEqual(rows, [(priority, problem, status_test, today_test_str, test_date_end, 2)])
 
 	def tearDown(self):
 		self.cur.execute('''DROP TABLE TODO''')
@@ -144,7 +143,7 @@ class test_db_update_priority(unittest.TestCase):
 		problem = 'Один'
 		status = 'Не выполнено'
 		date_today = datetime.date.today()
-		date_end = datetime.date(2020,12,4)
+		date_end = datetime.date(2020, 12, 4)
 		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
 		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
 
@@ -154,7 +153,7 @@ class test_db_update_priority(unittest.TestCase):
 		problem = 'Два'
 		status = 'Выполнено'
 		date_today = datetime.date.today()
-		date_end = datetime.date(2020,12,4)
+		date_end = datetime.date(2020, 12, 4)
 		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
 		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
 
@@ -169,10 +168,10 @@ class test_db_update_priority(unittest.TestCase):
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 1 ''')
 		rows = self.cur.fetchall()
 		priority_test = 'Нет'
-		test_date_end = str(datetime.date(2020,12,4))
+		test_date_end = str(datetime.date(2020, 12, 4))
 		problem = 'Один'
 		status = 'Не выполнено'
-		self.assertEqual(rows, [(priority_test,problem,status,today_test_str,test_date_end,1)])
+		self.assertEqual(rows, [(priority_test, problem, status, today_test_str, test_date_end, 1)])
 
 	def test_update_prior_task_2(self):
 		priority_ = 'Нет'
@@ -183,10 +182,10 @@ class test_db_update_priority(unittest.TestCase):
 		self.cur.execute('''SELECT * FROM TODO WHERE №= 2 ''')
 		rows = self.cur.fetchall()
 		priority_test = 'Важное'
-		test_date_end = str(datetime.date(2020,12,4))
+		test_date_end = str(datetime.date(2020, 12, 4))
 		problem = 'Два'
 		status = 'Выполнено'
-		self.assertEqual(rows, [(priority_test,problem,status,today_test_str,test_date_end,2)])
+		self.assertEqual(rows, [(priority_test, problem, status, today_test_str, test_date_end, 2)])
 
 	def tearDown(self):
 		self.cur.execute('''DROP TABLE TODO''')
@@ -212,7 +211,7 @@ class test_db_exp_tasks(unittest.TestCase):
 		problem = 'Один'
 		status = 'Не выполнено'
 		date_today = datetime.date.today()
-		date_end = datetime.date(2020,3,4)
+		date_end = datetime.date(2020, 3, 4)
 		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
 		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
 
@@ -224,7 +223,7 @@ class test_db_exp_tasks(unittest.TestCase):
 		rows_id = self.cur.fetchall()
 		for id_ in rows_id:
 			datetime_today = datetime.date.today()
-			date_end_test = datetime.date(2020,3,4)
+			date_end_test = datetime.date(2020, 3, 4)
 			exp_date = datetime_today - date_end_test
 			str_exp_tasks = 'Просрочено на ' + str(exp_date.days) + ' д'
 			self.assertEqual(id_[0], (str_exp_tasks))
@@ -253,7 +252,7 @@ class test_db_del_perf(unittest.TestCase):
 		problem = 'Один'
 		status = 'Не выполнено'
 		date_today = datetime.date.today()
-		date_end = datetime.date(2020,12,4)
+		date_end = datetime.date(2020, 12, 4)
 		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
 		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
 
@@ -263,7 +262,7 @@ class test_db_del_perf(unittest.TestCase):
 		problem = 'Два'
 		status = 'Выполнено'
 		date_today = datetime.date.today()
-		date_end = datetime.date(2020,12,4)
+		date_end = datetime.date(2020, 12, 4)
 		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
 		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
 
@@ -299,7 +298,7 @@ class test_db_clr_all(unittest.TestCase):
 		problem = 'Один'
 		status = 'Не выполнено'
 		date_today = datetime.date.today()
-		date_end = datetime.date(2020,12,4)
+		date_end = datetime.date(2020, 12, 4)
 		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
 		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
 
@@ -313,4 +312,3 @@ class test_db_clr_all(unittest.TestCase):
 
 if __name__ == "__main__":
 	unittest.main()
-
