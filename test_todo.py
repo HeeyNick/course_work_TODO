b0b7e7118ea360b8b1d9bc2ee9f091b85d132846
maksim <jeffrimorgan@yandex.ru>
2020-06-04 00:28:21 +0700

    Add class FuncDB, change setUp and tearDown all tests

--- test_todo.py
+++ test_todo.py
@@ -1,314 +1,229 @@
 import unittest
+import db_todo
 import datetime
 import sqlite3 as lite
-import db_todo
 
-class test_db_add_delete(unittest.TestCase):
-	def setUp(self):
-		self.db = db_todo.DB("test_db.db")
-		self.connection = lite.connect("test_db.db")
-		self.cur = self.connection.cursor()
-		self.cur.execute("""
-			CREATE TABLE IF NOT EXISTS TODO (
-  			Приоритет TEXT, 
-  			Задача TEXT NOT NULL,
- 			Статус TEXT ,
- 			Дата_добавления DATE,
- 			Дата_окончания DATE,
- 			№ INTEGER PRIMARY KEY AUTOINCREMENT
-			)
-			""")
+class FuncDB:
+	def create_db(self):
+		self.db = db_todo.DB('test_db.db')
+		self.status = db_todo.TaskStatus()
+		self.priority = db_todo.TaskPriority()
 
-		self.connection.commit()
+	def insert_db(self, priority, problem, status, date_end):
+		self.date_today = datetime.date.today()
+		self.db.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
+		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, self.date_today, date_end))
+
+		self.db.connection.commit()
+
+	def delete_db(self):
+		self.db.cur.execute('''DROP TABLE TODO''')
+		self.db.connection.commit()
+
+	def fetch_task_by_id(self, id_):
+		number = id_
+		number = tuple([number])
+		self.db.cur.execute('''SELECT * FROM TODO WHERE № = ? ''', (number))
+		rows = self.db.cur.fetchall()
+		return rows
+
+class TestAddDelete(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		self.func_db = FuncDB()
+		self.func_db.create_db()
 
 	def test_add_task_1(self):
-		date_end_test = datetime.date(2020, 3, 4)
-		date_today_test = datetime.date.today()
-		today_test_str = str(date_today_test)
-		problem = 'Трои'
-		self.db.execute_query(problem, date_today_test, date_end_test)
-		self.cur.execute('''SELECT * FROM TODO WHERE №= 1''')
-		rows = self.cur.fetchall()
-		self.assertEqual(rows, [('Нет', 'Трои', 'Не выполнено', today_test_str, '2020-03-04', 1)])
+		date_today = datetime.date.today()
+		date_end_test = datetime.date(2020,3,4)
+		today_test_str = str(date_today)
+		date_end = str(date_end_test)
+		problem ='Трои'
+		self.func_db.db.execute_query(problem, date_today, date_end_test)
+		id_ = 1
+		rows = self.func_db.fetch_task_by_id(id_)
+		self.assertEqual(rows, [(self.func_db.priority.normal, problem, self.func_db.status.unperf, today_test_str, date_end,1)])
 
 	def test_add_task_2(self):
-		date_end_test = datetime.date(2020, 3, 4)
-		date_today_test = datetime.date.today()
-		today_test_str = str(date_today_test)
-		problem = 'Ноль'
-		self.db.execute_query(problem, date_today_test, date_end_test)
-		self.cur.execute('''SELECT * FROM TODO WHERE №= 2''')
-		rows = self.cur.fetchall()
-		self.assertEqual(rows, [('Нет', 'Ноль', 'Не выполнено', today_test_str, '2020-03-04', 2)])
+		date_today = datetime.date.today()
+		date_end_test = datetime.date(2020,3,4)
+		today_test_str = str(date_today)
+		date_end = str(date_end_test)
+		problem ='Пять'
+		self.func_db.db.execute_query(problem, date_today, date_end_test)
+		id_ = 2
+		rows = self.func_db.fetch_task_by_id(id_)
+		self.assertEqual(rows, [(self.func_db.priority.normal, problem, self.func_db.status.unperf, today_test_str, date_end,2)])
 
 	def test_delete_task_1(self):
 		id_ = 1
-		self.db.delete_problem(id_)
-		self.cur.execute('''SELECT * FROM TODO WHERE №=1 ''')
-		rows_id = self.cur.fetchall()
-		self.assertEqual(rows_id, [])
+		self.func_db.db.delete_problem(id_)
+		rows = self.func_db.fetch_task_by_id(id_)
+		self.assertEqual(rows, [])
 
 	def test_delete_task_2(self):
 		id_ = 2
-		self.db.delete_problem(id_)
-		self.cur.execute('''SELECT * FROM TODO WHERE №=2''')
-		rows_id = self.cur.fetchall()
-		self.assertEqual(rows_id, [])
+		self.func_db.db.delete_problem(id_)
+		rows = self.func_db.fetch_task_by_id(id_)
+		self.assertEqual(rows, [])
 
-class test_db_update_perf(unittest.TestCase):
-	def setUp(self):
-		self.db = db_todo.DB("test_db.db")
-		self.connection = lite.connect("test_db.db")
-		self.cur = self.connection.cursor()
-		self.cur.execute("""
-			CREATE TABLE IF NOT EXISTS TODO (
-  			Приоритет TEXT, 
-  			Задача TEXT NOT NULL,
- 			Статус TEXT ,
- 			Дата_добавления DATE,
- 			Дата_окончания DATE,
- 			№ INTEGER PRIMARY KEY AUTOINCREMENT
-			)
-			""")
+	@classmethod
+	def tearDownClass(self):
+		self.func_db.delete_db()
 
-		priority = 'Нет'
-		problem = 'Один'
-		status = 'Не выполнено'
-		date_today = datetime.date.today()
-		date_end = datetime.date(2020, 12, 4)
-		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
-		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
 
-		self.connection.commit()
-
-		priority = 'Нет'
-		problem = 'Два'
-		status = 'Выполнено'
-		date_today = datetime.date.today()
-		date_end = datetime.date(2020, 12, 4)
-		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
-		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
-
-		self.connection.commit()
+class TestUpdatePerformed(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		self.func_db = FuncDB()
+		self.func_db.create_db()
 
 	def test_update_perf_task_1(self):
-		status_ = 'Не выполнено'
+		problem_insert = 'Один'
+		date_end_insert = datetime.date(2020,12,4)
+		self.func_db.insert_db(self.func_db.priority.normal, problem_insert, self.func_db.status.unperf, date_end_insert)
 		id_ = 1
-		date_today_test = datetime.date.today()
-		today_test_str = str(date_today_test)
-		self.db.update_performed(id_, status_)
-		self.cur.execute('''SELECT * FROM TODO WHERE №= 1 ''')
-		rows = self.cur.fetchall()
-		status_test = 'Выполнено'
-		test_date_end = str(datetime.date(2020, 12, 4))
-		problem = 'Один'
-		priority = 'Нет'
-		self.assertEqual(rows, [(priority, problem, status_test, today_test_str, test_date_end, 1)])
+		today_test_str = str(self.func_db.date_today)
+		self.func_db.db.update_performed(id_, self.func_db.status.unperf)
+		rows = self.func_db.fetch_task_by_id(id_)
+		test_date_end = str(datetime.date(2020,12,4))
+		problem_select = 'Один'
+		self.assertEqual(rows, [(self.func_db.priority.normal, problem_select, self.func_db.status.perf, today_test_str, test_date_end, 1)])
 
 	def test_update_perf_task_2(self):
-		status_ = 'Выполнено'
+		problem_insert = 'Два'
+		date_end_insert = datetime.date(2020,12,4)
+		self.func_db.insert_db(self.func_db.priority.normal, problem_insert, self.func_db.status.perf, date_end_insert)
 		id_ = 2
-		date_today_test = datetime.date.today()
-		today_test_str = str(date_today_test)
-		self.db.update_performed(id_, status_)
-		self.cur.execute('''SELECT * FROM TODO WHERE №= 2 ''')
-		rows = self.cur.fetchall()
-		status_test = 'Не выполнено'
-		test_date_end = str(datetime.date(2020, 12, 4))
-		problem = 'Два'
-		priority = 'Нет'
-		self.assertEqual(rows, [(priority, problem, status_test, today_test_str, test_date_end, 2)])
+		today_test_str = str(self.func_db.date_today)
+		self.func_db.db.update_performed(id_, self.func_db.status.perf)
+		rows = self.func_db.fetch_task_by_id(id_)
+		test_date_end = str(datetime.date(2020,12,4))
+		problem_select = 'Два'
+		self.assertEqual(rows, [(self.func_db.priority.normal, problem_select, self.func_db.status.unperf, today_test_str, test_date_end, 2)])
 
-	def tearDown(self):
-		self.cur.execute('''DROP TABLE TODO''')
-		self.connection.commit()
+	@classmethod
+	def tearDownClass(self):
+		self.func_db.delete_db()
 
-class test_db_update_priority(unittest.TestCase):
-	def setUp(self):
-		self.db = db_todo.DB("test_db.db")
-		self.connection = lite.connect("test_db.db")
-		self.cur = self.connection.cursor()
-		self.cur.execute("""
-			CREATE TABLE IF NOT EXISTS TODO (
-  			Приоритет TEXT, 
-  			Задача TEXT NOT NULL,
- 			Статус TEXT ,
- 			Дата_добавления DATE,
- 			Дата_окончания DATE,
- 			№ INTEGER PRIMARY KEY AUTOINCREMENT
-			)
-			""")
-
-		priority = 'Важное'
-		problem = 'Один'
-		status = 'Не выполнено'
-		date_today = datetime.date.today()
-		date_end = datetime.date(2020, 12, 4)
-		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
-		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
-
-		self.connection.commit()
-
-		priority = 'Нет'
-		problem = 'Два'
-		status = 'Выполнено'
-		date_today = datetime.date.today()
-		date_end = datetime.date(2020, 12, 4)
-		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
-		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
-
-		self.connection.commit()
+class TestUpdatePriority(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		self.func_db = FuncDB()
+		self.func_db.create_db()
 
 	def test_update_prior_task_1(self):
-		priority_ = 'Важное'
+		problem_insert = 'Один'
+		date_end_insert = datetime.date(2020,12,4)
+		self.func_db.insert_db(self.func_db.priority.major, problem_insert, self.func_db.status.unperf, date_end_insert)
 		id_ = 1
-		date_today_test = datetime.date.today()
-		today_test_str = str(date_today_test)
-		self.db.update_priority(id_, priority_)
-		self.cur.execute('''SELECT * FROM TODO WHERE №= 1 ''')
-		rows = self.cur.fetchall()
-		priority_test = 'Нет'
-		test_date_end = str(datetime.date(2020, 12, 4))
-		problem = 'Один'
-		status = 'Не выполнено'
-		self.assertEqual(rows, [(priority_test, problem, status, today_test_str, test_date_end, 1)])
+		today_test_str = str(self.func_db.date_today)
+		self.func_db.db.update_priority(id_, self.func_db.priority.major)
+		rows = self.func_db.fetch_task_by_id(id_)
+		test_date_end = str(datetime.date(2020,12,4))
+		problem_select = 'Один'
+		self.assertEqual(rows, [(self.func_db.priority.normal, problem_select, self.func_db.status.unperf, today_test_str, test_date_end, 1)])
 
 	def test_update_prior_task_2(self):
-		priority_ = 'Нет'
+		problem_insert = 'Два'
+		date_end_insert = datetime.date(2020,12,4)
+		self.func_db.insert_db(self.func_db.priority.normal, problem_insert, self.func_db.status.unperf, date_end_insert)
 		id_ = 2
-		date_today_test = datetime.date.today()
-		today_test_str = str(date_today_test)
-		self.db.update_priority(id_, priority_)
-		self.cur.execute('''SELECT * FROM TODO WHERE №= 2 ''')
-		rows = self.cur.fetchall()
-		priority_test = 'Важное'
-		test_date_end = str(datetime.date(2020, 12, 4))
-		problem = 'Два'
-		status = 'Выполнено'
-		self.assertEqual(rows, [(priority_test, problem, status, today_test_str, test_date_end, 2)])
+		today_test_str = str(self.func_db.date_today)
+		self.func_db.db.update_priority(id_, self.func_db.priority.normal)
+		rows = self.func_db.fetch_task_by_id(id_)
+		test_date_end = str(datetime.date(2020,12,4))
+		problem_select = 'Два'
+		self.assertEqual(rows, [(self.func_db.priority.major, problem_select, self.func_db.status.unperf, today_test_str, test_date_end, 2)])
 
-	def tearDown(self):
-		self.cur.execute('''DROP TABLE TODO''')
-		self.connection.commit()
+	@classmethod
+	def tearDownClass(self):
+		self.func_db.delete_db()
 
-class test_db_exp_tasks(unittest.TestCase):
+class TestExpTasks(unittest.TestCase):
+	@classmethod
 	def setUp(self):
-		self.db = db_todo.DB("test_db.db")
-		self.connection = lite.connect("test_db.db")
-		self.cur = self.connection.cursor()
-		self.cur.execute("""
-			CREATE TABLE IF NOT EXISTS TODO (
-  			Приоритет TEXT, 
-  			Задача TEXT NOT NULL,
- 			Статус TEXT ,
- 			Дата_добавления DATE,
- 			Дата_окончания DATE,
- 			№ INTEGER PRIMARY KEY AUTOINCREMENT
-			)
-			""")
-
-		priority = 'Нет'
-		problem = 'Один'
-		status = 'Не выполнено'
-		date_today = datetime.date.today()
-		date_end = datetime.date(2020, 3, 4)
-		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
-		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
-
-		self.connection.commit()
+		self.func_db = FuncDB()
+		self.func_db.create_db()
 
 	def test_expired_tasks(self):
-		self.db.expired_tasks_bd()
-		self.cur.execute('''SELECT Статус, Дата_окончания FROM TODO''')
-		rows_id = self.cur.fetchall()
+		problem_insert = 'Один'
+		date_today = datetime.date.today()
+		date_end_insert = datetime.date(2020,3,4)
+		self.func_db.insert_db(self.func_db.priority.normal, problem_insert, self.func_db.status.unperf, date_end_insert)
+		self.func_db.db.expired_tasks_bd()
+		self.func_db.db.cur.execute('''SELECT Статус, Дата_окончания FROM TODO''')
+		rows_id = self.func_db.db.cur.fetchall()
 		for id_ in rows_id:
 			datetime_today = datetime.date.today()
-			date_end_test = datetime.date(2020, 3, 4)
+			date_end_test = datetime.date(2020,3,4)
 			exp_date = datetime_today - date_end_test
 			str_exp_tasks = 'Просрочено на ' + str(exp_date.days) + ' д'
 			self.assertEqual(id_[0], (str_exp_tasks))
 
+	@classmethod
 	def tearDown(self):
-		self.cur.execute('''DROP TABLE TODO''')
-		self.connection.commit()
+		self.func_db.delete_db()
 
-class test_db_del_perf(unittest.TestCase):
-	def setUp(self):
-		self.db = db_todo.DB("test_db.db")
-		self.connection = lite.connect("test_db.db")
-		self.cur = self.connection.cursor()
-		self.cur.execute("""
-			CREATE TABLE IF NOT EXISTS TODO (
-  			Приоритет TEXT, 
-  			Задача TEXT NOT NULL,
- 			Статус TEXT ,
- 			Дата_добавления DATE,
- 			Дата_окончания DATE,
- 			№ INTEGER PRIMARY KEY AUTOINCREMENT
-			)
-			""")
+class TestDeletePerformed(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		self.func_db = FuncDB()
+		self.func_db.create_db()
 
-		priority = 'Нет'
-		problem = 'Один'
-		status = 'Не выполнено'
+	def test_delete_perf_1(self):
+		problem_insert_one = 'Один'
+		problem_insert_two = 'Два'
+		date_end_insert = datetime.date(2020,12,4)
+		test_date_end = str(datetime.date(2020,12,4))
+		self.func_db.insert_db(self.func_db.priority.normal, problem_insert_one, self.func_db.status.unperf, date_end_insert)
+		self.func_db.insert_db(self.func_db.priority.major, problem_insert_two, self.func_db.status.perf, date_end_insert)
+		self.func_db.db.delete_perfomed_in_bd()
+		id_ = 2
+		rows = self.func_db.fetch_task_by_id(id_)
+		self.assertEqual(rows, [])
+
+	def test_delete_perf_2(self):
+		problem_insert_five = 'Пять'
+		date_end_insert = datetime.date(2020,12,4)
+		test_date_end = str(datetime.date(2020,12,4))
 		date_today = datetime.date.today()
-		date_end = datetime.date(2020, 12, 4)
-		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
-		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
+		today_test_str = str(date_today)
+		self.func_db.insert_db(self.func_db.priority.normal, problem_insert_five, self.func_db.status.unperf, date_end_insert)
+		self.func_db.db.delete_perfomed_in_bd()
+		id_ = 3
+		rows = self.func_db.fetch_task_by_id(id_)
+		self.assertEqual(rows, [(self.func_db.priority.normal, problem_insert_five, self.func_db.status.unperf, today_test_str, test_date_end,3)])
 
-		self.connection.commit()
+	@classmethod
+	def tearDownClass(self):
+		self.func_db.delete_db()
 
-		priority = 'Нет'
-		problem = 'Два'
-		status = 'Выполнено'
-		date_today = datetime.date.today()
-		date_end = datetime.date(2020, 12, 4)
-		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
-		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
-
-		self.connection.commit()
-
-	def test_delete_perf(self):
-		self.db.delete_perfomed_in_bd()
-		self.cur.execute('''SELECT * FROM TODO WHERE №=2 ''')
-		rows_id = self.cur.fetchall()
-		self.assertEqual(rows_id, [])
-
-	def tearDown(self):
-		self.cur.execute('''DROP TABLE TODO''')
-		self.connection.commit()
-
-class test_db_clr_all(unittest.TestCase):
-	def setUp(self):
-		self.db = db_todo.DB("test_db.db")
-		self.connection = lite.connect("test_db.db")
-		self.cur = self.connection.cursor()
-		self.cur.execute("""
-			CREATE TABLE IF NOT EXISTS TODO (
-  			Приоритет TEXT, 
-  			Задача TEXT NOT NULL,
- 			Статус TEXT ,
- 			Дата_добавления DATE,
- 			Дата_окончания DATE,
- 			№ INTEGER PRIMARY KEY AUTOINCREMENT
-			)
-			""")
-
-		priority = 'Нет'
-		problem = 'Один'
-		status = 'Не выполнено'
-		date_today = datetime.date.today()
-		date_end = datetime.date(2020, 12, 4)
-		self.cur.execute('''INSERT INTO TODO(Приоритет, Задача, Статус, Дата_добавления, Дата_окончания) \
-		VALUES (?, ?, ?, ?, ?)''', (priority, problem, status, date_today, date_end))
-
-		self.connection.commit()
+class TestClearAll(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		self.func_db = FuncDB()
+		self.func_db.create_db()
 
 	def test_clear_all(self):
-		self.db.clear_all_problems()
-		self.cur.execute('''SELECT * FROM TODO''')
-		rows_id = self.cur.fetchall()
+		problem_insert_nine = 'Девять'
+		problem_insert_eight = 'Восемь'
+		problem_insert_six = 'Шесть'
+		date_end_insert = datetime.date(2020,12,4)
+		test_date_end = str(datetime.date(2020,12,4))
+		date_today = datetime.date.today()
+		today_test_str = str(date_today)
+		self.func_db.insert_db(self.func_db.priority.normal, problem_insert_nine, self.func_db.status.unperf, date_end_insert)
+		self.func_db.insert_db(self.func_db.priority.major, problem_insert_eight, self.func_db.status.perf, date_end_insert)
+		self.func_db.insert_db(self.func_db.priority.major, problem_insert_six, self.func_db.status.unperf, date_end_insert)
+		self.func_db.db.clear_all_problems()
+		self.func_db.db.cur.execute('''SELECT * FROM TODO''')
+		rows_id = self.func_db.db.cur.fetchall()
 		self.assertEqual(rows_id, [])
+
+	@classmethod
+	def tearDownClass(self):
+		self.func_db.delete_db()
 
 if __name__ == "__main__":
 	unittest.main()
